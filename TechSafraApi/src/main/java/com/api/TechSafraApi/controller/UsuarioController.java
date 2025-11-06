ed58ff292f12910db628be44d1ac977d19bc5167
AmarildoBlum1 <amarildoblum1234567@gmail.com>
2025-11-06 11:10:28 -0300

    .

--- TechSafraApi/src/main/java/com/api/TechSafraApi/controller/UsuarioController.java
+++ TechSafraApi/src/main/java/com/api/TechSafraApi/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.api.TechSafraApi.controller;
 
 import com.api.TechSafraApi.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -18,21 +20,20 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    @PostMapping("/cadastrar")
-    public String cadastrar(@RequestBody Map<String, String> body) {
-        String nome = body.get("nome");
-        String email = body.get("email");
-        String senha = body.get("senha");
-        String confirmarSenha = body.get("confirmarSenha");
-
-        return usuarioService.cadastrarUsuario(nome, email, senha, confirmarSenha);
-    }
-
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> body) {
+    public ResponseEntity<String> login(@RequestBody Map<String, String> body) {
         String email = body.get("email");
         String senha = body.get("senha");
 
-        return usuarioService.loginUsuario(email, senha);
+        String resultado = usuarioService.loginUsuario(email, senha);
+
+        switch (resultado) {
+            case "E-mail e senha são obrigatórios!":
+            case "E-mail não cadastrado!":
+            case "Senha incorreta!":
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resultado);
+            default:
+                return ResponseEntity.ok(resultado);
+        }
     }
 }
